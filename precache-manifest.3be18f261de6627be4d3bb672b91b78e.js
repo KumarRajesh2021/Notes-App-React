0bc14001dd0c2acec36f3f4fd11c907dc2308dcd
rajesh kumar <rajeshk200015@gmail.com>
2021-09-30 13:59:26 +0530

    Updates

---
+++ precache-manifest.3be18f261de6627be4d3bb672b91b78e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24689ac2f2862f0bba84179806e4315d",
+    "url": "/Notes-App-React/index.html"
+  },
+  {
+    "revision": "31edebf5a6ed2e0a5525",
+    "url": "/Notes-App-React/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3e3df2175c2958d25dbc",
+    "url": "/Notes-App-React/static/js/2.1a6086c9.chunk.js"
+  },
+  {
+    "revision": "31edebf5a6ed2e0a5525",
+    "url": "/Notes-App-React/static/js/main.b952208a.chunk.js"
+  },
+  {
+    "revision": "10a62f85cd1ae5d173bf",
+    "url": "/Notes-App-React/static/js/runtime-main.5a57a738.js"
+  }
+]);
